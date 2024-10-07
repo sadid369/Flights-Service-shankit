90d528ed1ceeab100957ffb6d36fca7673d2f58d
Sadid <66120003+sadid369@users.noreply.github.com>
2024-10-08 00:03:54 +0600

    info update

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -3,12 +3,12 @@ const { StatusCodes } = require('http-status-codes');
 const info = (req, res) => {
     return res.status(StatusCodes.OK).json({
         success: true,
-        message: 'API is live',
+        message: 'API is live in Flight Services',
         error: {},
         data: {},
     });
-}
+};
 
 module.exports = {
     info
-}
+};
